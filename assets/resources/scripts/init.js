e7764043cac51798c61b125928abdf9363a823ce
Jose Everaldo Bischof de Lima Junior <bischoflima@gmail.com>
2020-09-16 20:08:42 -0300

    modificado o rodape para carregar o ano automaticamente

--- assets/resources/scripts/init.js
+++ assets/resources/scripts/init.js
@@ -1,8 +1,3 @@
 $('.nav').load('../../../nav.html');
 $('.footer').load('../../../footer.html');
 
-function setDataAtual(){
-    'use strict';
-    let anoAtual = new Date();
-    document.getElementById('anoAtual').innerHTML = anoAtual.getFullYear();
-}
