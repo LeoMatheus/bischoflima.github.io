e8b05c405dcbd220552549cba19aa98dd867a2f9
Jose Everaldo Bischof de Lima Junior <bischoflima@gmail.com>
2020-09-16 19:36:44 -0300

    criadas as classes que representam o Usuario e o acesso ao localStorage

---
+++ assets/resources/scripts/classes.js
@@ -0,0 +1,55 @@
+let factoryUsuario = (email, nome, senha, telefone, cep) => {
+    'use strict';
+    return {
+        nome: nome,
+        email: email,
+        senha: senha,
+        telefone: telefone,
+        cep: cep
+    };
+};
+
+
+class BDUsuario {
+    constructor() {
+        'use strict';
+        let id = localStorage.getItem('id_cadastro');
+
+        if (id === null) {
+            localStorage.setItem('id_cadastro', 0);
+        }
+    }
+
+    getProximoId() {
+        'use strict';
+        return parseInt(localStorage.getItem('id_cadastro')) + 1;
+    }
+
+    gravar(usuario) {
+        'use strict';
+        let id = this.getProximoId();
+        localStorage.setItem('id_cadastro', id);
+        localStorage.setItem(id, JSON.stringify(usuario));
+    }
+
+    recuperarTodosRegistros() {
+        let qtdRegistros = parseInt(localStorage.getItem('id_cadastro'));
+        let usuarios = [];
+
+        for (let i = 1; i <= qtdRegistros; i++) {
+            if (localStorage.getItem(i) === null)
+                continue;
+            let usuario = JSON.parse(localStorage.getItem(i));
+            usuario.id = i;
+            usuarios.push(usuario);
+        }
+        return usuarios;
+    }
+
+    removerRegistro(idCadastro = undefined) {
+        if (idCadastro) {
+            localStorage.removeItem(`${idCadastro}`);
+        }
+    }
+
+}
