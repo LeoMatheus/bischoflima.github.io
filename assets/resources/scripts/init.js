d03c19708a621c200d500b6aeda2f99738505312
Jose Everaldo Bischof de Lima Junior <bischoflima@gmail.com>
2020-09-15 22:23:19 -0300

    modificado footer

--- assets/resources/scripts/init.js
+++ assets/resources/scripts/init.js
@@ -4,5 +4,5 @@ $('.footer').load('../../../footer.html');
 function setDataAtual(){
     'use strict';
     let anoAtual = new Date();
-    document.getElementById('anoAtual').textContent = anoAtual.getFullYear();
+    document.getElementById('anoAtual').innerHTML = anoAtual.getFullYear();
 }
