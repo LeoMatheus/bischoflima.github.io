d73d7a54125072cefbd5d30ba04bfe906915a609
Jose Everaldo Bischof de Lima Junior <bischoflima@gmail.com>
2020-09-17 11:35:39 -0300

    adicionada a validacao em modo tradicional no onkeypress do email

--- assets/resources/scripts/register.js
+++ assets/resources/scripts/register.js
@@ -1,4 +1,20 @@
+let validaEmail = (valorChar) => {
+    'use strict';
+    if (!(valorChar > 47 && valorChar < 58) && !(valorChar > 96 && valorChar < 123) &&
+        (valorChar !== 46) && (valorChar !== 64) && (valorChar !== 95))
+        return true;
+};
+
+document.querySelector('#email').onkeypress = function validaCaracter(event) {
+    'use strict';
+    if (validaEmail(event.charCode)) {
+        event.preventDefault();
+        console.log('inpedido de digitar');
+    }
+};
+
 let validacao = function () {
     'use strict';
     return false;
 };
+
