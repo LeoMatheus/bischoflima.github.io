68964ebbab5e4d06174cf9578b83c23f0f4471f3
Jose Everaldo Bischof de Lima Junior <bischoflima@gmail.com>
2020-09-17 14:12:56 -0300

    criada a validaçao no onblur das senhas

--- assets/resources/scripts/register.js
+++ assets/resources/scripts/register.js
@@ -9,12 +9,42 @@ document.querySelector('#email').onkeypress = function validaCaracter(event) {
     'use strict';
     if (validaEmail(event.charCode)) {
         event.preventDefault();
-        console.log('inpedido de digitar');
+        console.log('impedido de digitar');
     }
 };
 
+let tagEmail = document.getElementById('email');
+let tagNome = document.querySelector('#nome_usuario');
+let tagSenha1 = document.querySelector('#senha1');
+let tagSenha2 = document.querySelector('#senha2');
+let tagTelefone = document.querySelector('#phone');
+
+
+tagSenha2.onblur = () => {
+    'use strict';
+    let validaSenha = (senha1, senha2) => {
+        if (senha1)
+            if (senha2)
+                if (senha1 === senha2)
+                    return true;
+
+        return false;
+
+    };
+
+    if (validaSenha(tagSenha1.value, tagSenha2.value)) {
+        console.log('ira apresentar abaixo do input de senha "as senhas coincidem"');
+    } else {
+        alert('As senhas não coincidem!!\nOu o valor é inválido.');
+    }
+
+};
+
+
 let validacao = function () {
     'use strict';
+
+    console.log(tagEmail.value, tagNome.value, tagSenha1.value, tagSenha2.value, tagTelefone.value);
     return false;
 };
 
